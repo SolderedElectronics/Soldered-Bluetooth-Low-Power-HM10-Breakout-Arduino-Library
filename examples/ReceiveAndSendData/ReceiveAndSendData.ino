f64d06bc02435c96a4bf4c5aed7a96b84f4f2c2d
Main Push Robot <robot@example.com>
2022-02-23 08:22:56 +0000

    Committing dev changes

--- examples/ReceiveAndSendData/ReceiveAndSendData.ino
+++ examples/ReceiveAndSendData/ReceiveAndSendData.ino
@@ -13,48 +13,52 @@
 
 #include "BLE-SOLDERED.h"
 
-// Define pins as transmitter and receiver, must be same as pins that module is connected to Soldered Microcontroller
-int BT_RX = 2;
-int BT_TX = 3;
+// Define pins as transmitter and receiver
+// must be same as pins that module is connected to Soldered Microcontroller
+int BT_RX = 2;  //This library is based on SoftwareSerial library,
+int BT_TX = 3;  //so refer to documentation of that library for more details
 
-BLE bt(BT_RX, BT_TX);
+BLE bt(BT_RX, BT_TX); //Create instance of class and specify pins for UART 
+                      //communication with BLE module
 
 String msg;
 
 // LED initialisation
-int LED = USER_LED;
+#define LED 9
 
 void setup()
 {
-    bt.begin(9600);
+    bt.begin(9600);  //Start serial communication with BLE module using 9600 bauds/s
     bt.println("Bt is on");
-    pinMode(LED, OUTPUT);
-}
+    pinMode(LED, OUTPUT);  //Set pin LED to OUTPUT mode,
+                           //so LED can be turned on and off
+} 
 // Wait for message
 // analise it and then either turn LED on or off
 
 void loop()
 {
-    if (bt.available())
-    {                          // data available
+    if (bt.available())        //Check if BLE is available for communication
+    {
         msg = bt.readString(); // Read data
-        if (msg == "on")
+        if (msg == "on")  //If received message is "on"
         {
-            digitalWrite(LED, HIGH);
-            bt.print("LED");
+            digitalWrite(LED, HIGH);  //Turn on LED
+            bt.print("LED");          //Send feedback information
             bt.print(LED);
             bt.println("On!");
         }
-        else if (msg == "off")
+        else if (msg == "off")  //If received message is "off"
         {
-            digitalWrite(LED, LOW);
-            bt.print("LED");
+            digitalWrite(LED, LOW);  //Turn on LED
+            bt.print("LED");         //Send feedback information
             bt.print(LED);
             bt.println("Off!");
         }
         else
         {
-            bt.print("N/A"); // send unknown message arived
+            bt.print("N/A"); // Send feedback information that
+                             // unknown command is received
         }
     }
 }
