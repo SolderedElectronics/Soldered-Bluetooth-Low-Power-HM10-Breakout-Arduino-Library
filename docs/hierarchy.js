d188b5ad0b871fb22ca8f836102042ae27a752c5
Docs Robot <robot@example.com>
2022-02-23 13:21:36 +0000

    Committing documentation changes

--- docs/hierarchy.js
+++ docs/hierarchy.js
@@ -1,5 +1,8 @@
 var hierarchy =
 [
+    [ "HardwareSerial", null, [
+      [ "BLE", "class_b_l_e.html", null ]
+    ] ],
     [ "SoftwareSerial", null, [
       [ "BLE", "class_b_l_e.html", null ]
     ] ]
